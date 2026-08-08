bc7f0dba7b9523e435a0a3531369c7a97fc74baa
Voctl <denis.3131a@gmail.com>
2026-08-08 23:50:18 +0400

    doddles and some shi to understand this shi

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,18 @@ mod eng;
 use eng::lexer::*;
 
 
+// lim(var, val, exp)
 enum Expr{
-    Sym(String),
+    Symb(String),
     Func(String , Vector<Expr>)
 }
+
+// lefts side-> a = a <- right side
+struct Rules {
+    leftex : Expr,
+    rightex : Expr,
+}
+
 
 fn main() {
     println!("main");
