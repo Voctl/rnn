25e02f6ffac92fb3883f6cd5f7e701355a5538ab
Voctl <denis.3131a@gmail.com>
2026-08-08 21:16:36 +0400

    doddles and some shi to understand this shi

---
+++ src/eng/lexer.rs
@@ -0,0 +1,5 @@
+// TODO : write a lexer here
+
+pub fn main(){
+    println!("there will be a lexer");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,7 @@
-use std::io;
-
-enum Token {
-    Eded(f64),
-    Plus,     // '+'
-    Minus,    // '-'
-    Multiply, // '*'
-    Divide,   // '/'
-    LParen,   // '('
-    RParen,   // ')'
-}
-
-// lexer
-fn lexer() -> Vec<Token> {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read");
-
-    let mut tokens = Vec::new();
-    for c in input.
-        // TODO: Burada `match c` yazıb simvolları tokena cevir
-    }
-}
-
-//parser
+mod eng;
+use eng::lexer::*;
 
 
-//exec
 fn main() {
+    println!("main");
 }
