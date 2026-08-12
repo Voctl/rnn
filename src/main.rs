b33a87c2384f343ef18baf78e8c2422ece123bec
Voctl <denis.3131a@gmail.com>
2026-08-12 04:19:29 +0400

    working on pattern matching

--- src/main.rs
+++ src/main.rs
@@ -102,7 +102,7 @@ fn main() {
     };
 
     // Pattern swap(pair(a, b))
-    let pattern = &swap.leftex;
+    let pattern = &swap.rightex;
     // Value swap(pair(f(c), g(d)))
     let value = Func(
         "swap".to_string(),
